972a6dd50c64aa3ea746a962844da30f52edb06f
DBurby <burby@hartford.edu>
2020-05-12 23:03:30 -0400

    add click event to unrotate other letters when clicking a new one

--- js/app.js
+++ js/app.js
@@ -6,4 +6,33 @@ window.onload = () => {
             l[i].classList.toggle(`rotate`);
         });
     }
+
+    l[0].addEventListener(`click`, () => {
+        if (l[0].classList.contains(`rotate`)){
+            l[1].classList.remove(`rotate`);
+            l[2].classList.remove(`rotate`);
+            l[3].classList.remove(`rotate`);
+        }
+    });
+    l[1].addEventListener(`click`, () => {
+        if (l[1].classList.contains(`rotate`)){
+            l[0].classList.remove(`rotate`);
+            l[2].classList.remove(`rotate`);
+            l[3].classList.remove(`rotate`);
+        }
+    });
+    l[2].addEventListener(`click`, () => {
+        if (l[2].classList.contains(`rotate`)){
+            l[0].classList.remove(`rotate`);
+            l[1].classList.remove(`rotate`);
+            l[3].classList.remove(`rotate`);
+        }
+    });
+    l[3].addEventListener(`click`, () => {
+        if (l[3].classList.contains(`rotate`)){
+            l[0].classList.remove(`rotate`);
+            l[1].classList.remove(`rotate`);
+            l[2].classList.remove(`rotate`);
+        }
+    });
 };
